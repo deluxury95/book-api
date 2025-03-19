c1b981ae7fa4703388eaed1735c804c2767219d5
dengluxurye <dluxurymou@gmail.comgit config --global user.email dluxurymou@gmail.com>
2025-03-19 13:56:01 +0200

    i made a changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 
 const mongoose = require("mongoose");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 // middleware

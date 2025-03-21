42a84893e7866b05163633a11b7cb0a299fc9750
dengluxurye <dluxurymou@gmail.comgit config --global user.email dluxurymou@gmail.com>
2025-03-21 07:40:46 +0200

    ok

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: true
+// }));
 
 // Routes
 const bookRoutes = require('./src/books/book.route.js');
